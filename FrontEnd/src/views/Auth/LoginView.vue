b6f68a122c6043ad92a39896e96d73a06be8713f
Tiago <tiagodinissilva26@gmail.com>
2024-02-07 16:24:32 +0000

    Login and Register Finish

--- FrontEnd/src/views/Auth/LoginView.vue
+++ FrontEnd/src/views/Auth/LoginView.vue
@@ -1,2 +1,23 @@
 <template>
+    <div class="wrapper">
+        <div class="form-box login">
+            <h2>Login</h2>
+            <form action="#">
+                <div class="input-box">
+                    <span class="icon"><ion-icon name="mail"></ion-icon></span>
+                    <input type="email" required>
+                    <label>Email</label>
+                </div>
+                <div class="input-box">
+                    <span class="icon"><ion-icon name="lock-closed"></ion-icon></span>
+                    <input type="password" required>
+                    <label>Password</label>
+                </div>
+                <button type="submit" class="btn">Login</button>
+                <div class="login-register">
+                    <p>Don't have an account? <a href="/register" class="register-link">Register</a></p>
+                </div>
+            </form>
+        </div>
+    </div>
 </template>
---
+++ FrontEnd/src/views/Auth/RegisterView.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="wrapper-register">
+        <div class="form-box login">
+            <h2>Register</h2>
+            <form action="#">
+                <div class="input-box">
+                    <span class="icon"><ion-icon name="person"></ion-icon></span>
+                    <input type="text" required>
+                    <label>Name</label>
+                </div>
+                <div class="input-box">
+                    <span class="icon"><ion-icon name="mail"></ion-icon></span>
+                    <input type="email" required>
+                    <label>Email</label>
+                </div>
+                <div class="input-box">
+                    <span class="icon"><ion-icon name="lock-closed"></ion-icon></span>
+                    <input type="password" required>
+                    <label>Password</label>
+                </div>
+                <div class="input-box">
+                    <span class="icon"><ion-icon name="repeat-outline"></ion-icon></span>
+                    <input type="password" required>
+                    <label>Repeat Password</label>
+                </div>
+                <button type="submit" class="btn">Register</button>
+                <div class="login-register">
+                    <p>You already have account? <a href="/login" class="register-link">Login</a></p>
+                </div>
+            </form>
+        </div>
+    </div>
+</template>
